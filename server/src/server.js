576dbc18f15171f7858aedabcf67831408a4352a
Proteseus <lewiwui@gmail.com>
2025-04-26 12:04:25 +0300

    cors added

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,10 @@ const prisma = new PrismaClient({
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [process.env.CORS_ORIGIN, 'http://localhost:4000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(requestLogger);
 
