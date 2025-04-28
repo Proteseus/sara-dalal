b93df96cb784572917cd8a76921fa98068c0891d
Proteseus <lewiwui@gmail.com>
2025-04-28 16:19:58 +0300

    fix

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { sendGreetingEmail } from '../utils/notifications';
+import { sendGreetingEmail } from '../utils/notifications.js';
 
 const prisma = new PrismaClient();
 
@@ -38,7 +38,7 @@ export const registerUser = async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRES_IN }
     );
-    
+
     await sendGreetingEmail(user.email, user.firstName);
 
     res.status(201).json({
